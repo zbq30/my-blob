1d092ffe916120a8e6872224908d4be50a537392
zbq30 <943912367@qq.com>
2023-03-02 01:38:25 +0800

    修改登录弹窗样式&新增倒计时组件

--- my-blob/components/Login/index.tsx
+++ my-blob/components/Login/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next"
 import styles from "./index.module.scss"
-import { useState } from "react"
+import CountDown from "components/CountDown";
+import { ChangeEvent, useState } from "react"
+import { GithubOutlined } from '@ant-design/icons';
 
 
 interface IProps {
@@ -10,6 +12,7 @@ interface IProps {
 
 const Login = (props: IProps) => {
     console.log('props---------', props);
+    const [isShowVerifyCode, setIsShowVerifyCode] = useState(false)
     const [form, setFrom] = useState({
         phone: '',
         verify: '',
@@ -19,7 +22,7 @@ const Login = (props: IProps) => {
 
     }
     const handleGetVerityCode = () => {
-
+        setIsShowVerifyCode(true)
     }
     const handleLogin = () => {
 
@@ -27,24 +30,55 @@ const Login = (props: IProps) => {
     const handleoAuthGithub = () => {
 
     }
+    const handleCountDownEnd = () => {
+        setIsShowVerifyCode(false)
+    }
+    const handleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+        // eslint-disable-next-line no-unsafe-optional-chaining
+        const { name, value } = e?.target
+        setFrom({
+            ...form,
+            [name]: value
+        })
+    }
     const { isShow = false } = props
     return isShow ? (
         <div className={styles.loginArea}>
             <div className={styles.loginBox}>
                 <div className={styles.loginTitle}>
                     <div>手机号登录</div>
-                    <div className={styles.close} onClick={handleClose}>x</div>
-                    <input name="phone" type='text' placeholder="请输入手机号" value={form.phone} />
-                    <div className={styles.verifyCodeArea}>
-                        <input name="verify" type='text' placeholder="请输入验证码" value={form.verify} />
-                        <span className={styles.verityCode} onClick={handleGetVerityCode}>获取验证码</span>
+                    <div className={styles.close} onClick={handleClose}>
+                        x
                     </div>
-                    <div className={styles.loginBtn} onClick={handleLogin}>登录</div>
-                    <div className={styles.otherLogin} onClick={handleoAuthGithub}>使用 Github 登录</div>
-                    <div className={styles.loginPrivacy}>
-                        注册登录即表示同意
-                        <a href="https://moco.imooc.com/primary.html" target='_blank' rel="noreferrer">隐私政策</a>
-                    </div>
+                </div>
+                <input
+                    name="phone"
+                    type='text'
+                    placeholder="请输入手机号"
+                    value={form.phone}
+                    onChange={handleFormChange}
+                />
+                <div className={styles.verifyCodeArea}>
+                    <input
+                        name="verify"
+                        type='text'
+                        placeholder="请输入验证码"
+                        value={form.verify}
+                        onChange={handleFormChange}
+                    />
+                    <span className={styles.verifyCode} onClick={handleGetVerityCode}>
+                        {isShowVerifyCode ? <CountDown time={10} onEnd={handleCountDownEnd} /> : '获取验证码'}
+                    </span>
+                </div>
+                <div className={styles.loginBtn} onClick={handleLogin}>
+                    登录
+                </div>
+                <div className={styles.otherLogin} onClick={handleoAuthGithub}>
+                    <GithubOutlined />使用 Github 登录
+                </div>
+                <div className={styles.loginPrivacy}>
+                    注册登录即表示同意
+                    <a href="https://moco.imooc.com/primary.html" target='_blank' rel="noreferrer">隐私政策</a>
                 </div>
             </div>
         </div>
